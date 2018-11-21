9ccf432b6dc1a6154766fcd862f8c72eca7151b7
iftekhar joy <chowdhuryiftekharulislam@outlook.com>
2018-11-21 23:48:04 +0600

    errors file modified

---
+++ app/main/errors.py
@@ -0,0 +1,11 @@
+from flask import render_template
+
+from . import main 
+
+@main.app_errorhandler(4o4)
+def page_not_found(e):
+	return render_template('404.html'), 404 
+
+@main.app_errorhandler(500)
+def interval_server_error(e):
+	return render_template('500.html'), 500 
