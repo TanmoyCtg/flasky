b54a56415d2eee15317500baf44e58985293aa71
iftekhar joy <chowdhuryiftekharulislam@outlook.com>
2018-11-24 23:27:02 +0600

    modified

--- app/__init__.py
+++ app/__init__.py
@@ -6,17 +6,13 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config 
 
 bootstrap = Bootstrap()
-
 mail = Mail()
-
 moment = Moment() 
-
 db = SQLAlchemy()
 
 def create_app(config_name):
 
-	from main import main as main_blueprint
-
+	
 	app = Flask(__name__)
 	app.config.from_object(config[config_name])
 	config[config_name].init_app(app)
@@ -26,6 +22,7 @@ def create_app(config_name):
 	moment.init_app(app)
 	db.init_app(app) 
 
+	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
 
 	return app 
